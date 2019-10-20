645c9a5321549faa0d5366a45b18f4e2db1e1c79
Niccolò Biondi <niccolo.biondi1@stud.unifi.it>
2019-10-20 16:52:58 +0200

    update

--- tools/preprocess.py
+++ tools/preprocess.py
@@ -1,4 +1,4 @@
-import os, cv2, shutil, argparse, sys
+import os, cv2, shutil, argparse, sys, tqdm
 sys.path.insert(1, os.path.dirname(os.path.abspath('__file__')))
 from PIL import Image
 from config.config import cfg
@@ -22,7 +22,7 @@ def adjustImages(data_type):
     for seq in sorted(os.listdir(input_path)):
         if seq != ".DS_Store":
             image_path = input_path + seq + "/left/"
-            output = output_path + type + "/" + seq + "/"
+            output = output_path + data_type + "/" + seq + "/"
 
             if not os.path.exists(output):
                 os.makedirs(output)
@@ -37,7 +37,7 @@ def adjustImages(data_type):
             print('*' * 100)
             print('Analisi Sequenza {n_seq}/{num_seq} in corso ...'.format(n_seq=i,num_seq=num_seq))
             print("Generazione Nuove Immagini dall'Input:", image_path)
-            for image in sorted(os.listdir(image_path)):
+            for image in tqdm.tqdm(sorted(os.listdir(image_path))):
                 if (image.endswith(".png")):  # Controlla che si tratti di un'immagine
                     img = cv2.imread(image_path + '{img_name}'.format(img_name=image), cv2.IMREAD_UNCHANGED)
                     height = img.shape[0]
@@ -48,14 +48,12 @@ def adjustImages(data_type):
                     dim = (new_width, new_height)
                     resized = cv2.resize(crop_img, dim, interpolation=cv2.INTER_AREA)
 
-                    print('Sequenza seq{n_seq}, Image {img} Saving'.format(n_seq=i, img=image))
-
                     cv2.imwrite(os.path.join(output, image), resized)
             print('Output Nuove Immagini:', output)
             print('*' * 100)
             i += 1
 
-def adjustDepth(input_path, output_path, data_type):
+def adjustDepth(data_type):
 
     dir_name = os.path.dirname(os.path.abspath(__file__))
 
@@ -72,7 +70,7 @@ def adjustDepth(input_path, output_path, data_type):
     for seq in sorted(os.listdir(input_path)):
         if seq != ".DS_Store":
             depths_input = input_path + seq + "/depth/"
-            output = output_path + type + "/" + seq + "/"
+            output = output_path + data_type + "/" + seq + "/"
 
             if not os.path.exists(output):
                 os.makedirs(output)
@@ -85,7 +83,7 @@ def adjustDepth(input_path, output_path, data_type):
         print('*' * 100)
         print('Analisi Sequenza {n_seq}/{num_seq} in corso ...'.format(n_seq=i,num_seq=num_seq))
         print("Generazione Nuove Immagini dall'Input:", depths_input)
-        for image in sorted(os.listdir(depths_input)):
+        for image in tqdm.tqdm(sorted(os.listdir(depths_input))):
             if (image.endswith('png')):  # Controlla che si tratti di un'immagine
                 img = cv2.imread(depths_input + '{img_name}'.format(img_name=image), cv2.IMREAD_UNCHANGED)
                 height = img.shape[0]
@@ -95,8 +93,6 @@ def adjustDepth(input_path, output_path, data_type):
                 new_height = int(height * SCALE_PERCENT / 100)
                 dim = (new_width, new_height)
                 resized = cv2.resize(crop_img, dim, interpolation=cv2.INTER_AREA)
-
-                print('Sequenza seq{n_seq}, Image {img} Saving'.format(n_seq=i, img=image))
 
                 cv2.imwrite(os.path.join(output, image), resized)
         print('Output Nuove Immagini:', output)
